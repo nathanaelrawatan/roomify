d1c969dadee868e017d2781926d6856dc01d88e1
nathanaelrawatan <david.rawatan@gmail.com>
2026-03-04 12:11:21 +0800

    Compare visualizer

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -4,6 +4,7 @@ import {generate3DView} from "../../lib/ai.action";
 import {Box, Download, RefreshCcw, Share2, X} from "lucide-react";
 import {Button} from "../../components/ui/button";
 import {createProject, getProjectById} from "../../lib/puter.action";
+import {ReactCompareSlider, ReactCompareSliderImage} from "react-compare-slider";
 
 const Visualizer = () => {
     const { id } = useParams()
@@ -19,6 +20,15 @@ const Visualizer = () => {
     const [currentImage, setCurrentImage] = useState<string | null>(null)
 
     const handleBack = () => navigate('/')
+    const handleExport = () => {
+        if (!currentImage) return;
+        const link = document.createElement('a');
+        link.href = currentImage;
+        link.download = `roomify-${id || 'design'}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 
     const runGeneration = async (item: DesignItem) => {
         if (!id || !item.sourceImage) return;
@@ -124,7 +134,7 @@ const Visualizer = () => {
                         <div className="panel-actions">
                             <Button
                                 size="sm"
-                                onClick={() => {}}
+                                onClick={handleExport}
                                 className="export"
                                 disabled={!currentImage}
                                 >
@@ -161,6 +171,42 @@ const Visualizer = () => {
                             </div>
                         )}
                     </div>
+                </div>
+
+                <div className="panel compare">
+                    <div className="panel-header">
+                        <div className="panel-meta">
+                            <p>Comparison</p>
+                            <h3>Before and After</h3>
+
+                        </div>
+                        <div className="hint">
+                            Drag to compare
+                        </div>
+                    </div>
+
+                    <div className="compare-stage">
+                        {project?.sourceImage && currentImage
+                            ? (
+                                <ReactCompareSlider
+                                    defaultValue={50}
+                                    style={{width: '100%', height: 'auto'}}
+                                    itemOne={<ReactCompareSliderImage src={project.sourceImage} alt="Before" className="compare-img" />}
+                                    itemTwo={<ReactCompareSliderImage src={currentImage || project?.renderedImage} alt="Before" className="compare-img" />}
+                                />
+                            ): (
+                                <div className="compare-fallback">
+                                    {project?.sourceImage && (
+                                        <img src={project.sourceImage}  alt="Before" className="compare-img"/>
+                                    )
+
+                                    }
+                                </div>
+
+                            )}
+
+                    </div>
+
                 </div>
             </section>
         </div>
